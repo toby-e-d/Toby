1188d9640dab50711344cb43dc06023477d4d00d
toby-e-d <63106705+toby-e-d@users.noreply.github.com>
2020-04-03 14:06:58 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+let name = "Toby";
+let age = "14";
+let school = "Harlington Upper";
+
+console.log("Hi, my name is " + name);
+console.log("I am " + age + " years old.")
+console.log("I go to " + school + " School.")
+
+
+let numberone;
+let numbertwo;
+let total;
+
+// This (below) is an arrow function
+const addition = (numberone, numbertwo) => {
+    total = numberone + numbertwo
+    console.log(total);
+};
+
+addition(20, 30);
+addition(30, 70);
+addition(40, 15);
+addition(754, 8927);
+
+
+
